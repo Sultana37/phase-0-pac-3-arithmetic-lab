a9ed77d2c3e254ae16bbe0c3d15301ec36fc7ee2
Esrat <imranzaman.318@gmail.com>
2021-03-14 01:22:38 -0500

    Completed Done

---
+++ index.js
@@ -0,0 +1,31 @@
+ 
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+var n =1;
+
+    function add(a,b){
+      return a+b;
+    }
+    function subtract (a,b){
+      return a-b;
+    }
+    function multiply(a,b){
+      return a*b;
+    }
+    function divide(a,b){
+      return a/b;
+    }
+    function increment(n){
+      return n+=1;
+    }
+    function decrement (n){
+      return n-=1;
+    }
+    function makeInt(n){
+      return parseInt(String);
+    }
+    function preserveDecimal(n) {
+      return parseFloat(String);
+    }
+    
+
